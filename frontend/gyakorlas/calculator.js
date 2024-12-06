de8c9787c5cc94d07cf152b30dd8c5ccec8fa8df
Akos032 <horako032@hengersor.hu>
2024-12-06 10:46:11 +0100

    update

---
+++ frontend/gyakorlas/calculator.js
@@ -0,0 +1,5 @@
+function add(num, num2){
+    return num1 + num2;
+}
+
+module.exports = add;
--- frontend/gyakorlas/index.js
+++ frontend/gyakorlas/index.js
@@ -5,7 +5,12 @@
     console.log(__dirname);
     console.log(__filename);
 
+    const add = reguire('./calculator.js');
+    console.log(add(2,3));
+
+    require('http');
 //}
+
 
 
 
