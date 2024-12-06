2e5f0aac9e7a12c8f40f1523b57479878276d8fc
Akos032 <horako032@hengersor.hu>
2024-12-06 11:02:47 +0100

    2024.12.06
    note js server

--- frontend/gyakorlas/index.js
+++ frontend/gyakorlas/index.js
@@ -2,13 +2,22 @@
 //wrapper function
 // function(__dirname, __filename) {
 
+const { Server } = require('http');
+
     console.log(__dirname);
     console.log(__filename);
 
     const add = reguire('./calculator.js');
-    console.log(add(2,3));
+    console.log(add(2,3)); 
 
     require('http');
+
+    const server = http.createServer(function(req,res){
+        res.writeHead(200);
+        res.end('Hello, World!');
+    });
+
+    server.lister(8080);
 //}
 
 
