b5c1fecaaed369c3e2f913a71348243d9d78ce79
Akos032 <horako032@hengersor.hu>
2024-12-09 12:24:35 +0100

    v2

--- react/app.js
+++ react/app.js
@@ -10,3 +10,15 @@ btn.onclick = function()
 btn.innerHTML = "Natív"
 
 document.getElementById("nativ-button-container").appendChild(btn);
+
+const gomb = React.createElement("button", 
+    {
+        onclick: function()
+        {
+            alert("Hello");
+        },
+    },
+    "React gomb"
+)
+
+ReactDOM.render(gomb, document.getElementById("react-button-containe"))
