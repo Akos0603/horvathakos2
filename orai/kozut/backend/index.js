d7154aae966d82bd5e1636b0c05ae8b3f11d1d94
Akos032 <horako032@hengersor.hu>
2025-01-24 12:24:10 +0100

    /

--- orai/kozut/backend/index.js
+++ orai/kozut/backend/index.js
@@ -5,3 +5,27 @@ const mysql = require("mysql");
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(cors());
+
+const db = mysql.createConnection({
+    user: "root",
+    host: "127.0.0.1",
+    port: 3306,
+    password: "",
+    database:"kozutak",
+});
+
+app.get("/", (req,res) => {
+    res.send("Fut a backend");
+})
+
+app.get("/user", (req,res) =>{
+    const sql = "Select * from `regiok`";
+    db.query(sql, (err, result) =>{
+        if(err) return res.json(err);
+        return res.json(result)
+    })
+})
+
+app.listen(3001, () =>{
+    console.log("Server is running ob port 3001");
+})
