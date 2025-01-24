102c49c45d1a1ab642904609dde46680a296e41c
Akos032 <horako032@hengersor.hu>
2025-01-24 14:19:02 +0100

    Update index.js

--- orai/kozut/backend/index.js
+++ orai/kozut/backend/index.js
@@ -26,6 +26,22 @@ app.get("/user", (req,res) =>{
     })
 })
 
+app.get("/8_id", (req,res) =>{
+    const sql = "Select * from `regiok` where Rid = 8";
+    db.query(sql, (err,result) => {
+        if(err) return res.json(err);
+        return res.json(result)
+    })
+})
+
+app.get("/?_id", (req,res) =>{
+    const sql = "Select * from `regiok` where Rid = 8";
+    db.query(sql, (err,result) => {
+        if(err) return res.json(err);
+        return res.json(result)
+    })
+})
+
 app.listen(3001, () =>{
     console.log("Server is running ob port 3001");
 })
