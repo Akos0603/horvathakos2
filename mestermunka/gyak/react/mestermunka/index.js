3c827848fccb6016dfef52e06b61ddf9f2f01401
Akos032 <horako032@hengersor.hu>
2025-02-03 11:56:38 +0100

    fronted

---
+++ mestermunka/gyak/react/mestermunka/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mysql = require("mysql");
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(cors());
+
+const db = mysql.createConnection({
+    user: "root",
+    host:"127.0.0.1",
+    port: 3306,
+    password: "",
+    database: "finomsagok"
+
+})
+
+app.get("/", (req,res) => {
+    res.send("Fut a backend");
+})
+
+app.get("/*" , (req,res) => {
+    const sql = "Select * from `receptek`";
+    db.query(sql, (err, result) =>{
+        if(err) return res.json(err)
+        return res.json(result)
+    })
+}) 
+
+app.listen(3000, () => {
+    console.log("Server is running ob port 3000");
+})
